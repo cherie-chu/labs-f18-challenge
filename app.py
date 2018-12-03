f1cdfa282da5268c83e3cc4d093d9833e7c2d099
Cherie Chu <cheriechu@dyn-209-2-221-64.dyn.columbia.edu>
2018-12-02 21:22:13 -0500

    created pokemon pages for name and id queries

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -7,6 +8,15 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/pokemon/<query>', methods=['GET'])
+def catch_pokemon(query):
+	profile = requests.get('https://pokeapi.co/api/v2/pokemon/' + query).json()
+	pokemon = profile['name']
+	num = str(profile['id'])
+	if query.isdigit():
+		return render_template('pokemon.html', pokemon=pokemon, id=query)
+	else:
+		return render_template('pokemon2.html', pokemon=query, id=num)
 
 if __name__ == '__main__':
     app.run()
